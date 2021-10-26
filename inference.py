061ed64369356c43a4c3bee7c3b1d0e7fbcd79f3
Jongho Park <jhzpon9@gmail.com>
2021-10-26 19:36:14 +0900

    `output_dir` arg defaults to 'predictions'

--- inference.py
+++ inference.py
@@ -22,7 +22,7 @@ def parse_args():
     # Conventional args
     parser.add_argument('--data_dir', default=os.environ.get('SM_CHANNEL_EVAL'))
     parser.add_argument('--model_dir', default=os.environ.get('SM_CHANNEL_MODEL', 'trained_models'))
-    parser.add_argument('--output_dir', default=os.environ.get('SM_OUTPUT_DATA_DIR'))
+    parser.add_argument('--output_dir', default=os.environ.get('SM_OUTPUT_DATA_DIR', 'predictions'))
 
     parser.add_argument('--device', default='cuda' if cuda.is_available() else 'cpu')
     parser.add_argument('--input_size', type=int, default=1024)
